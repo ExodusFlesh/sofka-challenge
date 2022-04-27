5bcbad16e4c87115e0d2ef609184e3c3eba05c5f
Steve_R <stebanram25@gmail.com>
2022-04-27 16:29:14 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className='container'>
     <Router>
-      <Route path="/" component={Main}/>
+      <Route path="/" exact component={Main}/> 
       <Route path="/game" component={Game}/>
     </Router>
     </div>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
-const Main = () => {
-  return (
+export default class Main extends Component {
+  render() {
+    return (
     <div className='containerTitle'>
       <div className='nameGame'>
         <h1>Challenge <span>Sofka</span></h1>
       </div>
       <div className='player'>
         <textarea placeholder='Nombre' className='playerName'></textarea>
-        <Link className='playerStart' to="/start"> Jugar Ahora</Link>
+        <Link className='playerStart' to="/game"> Jugar Ahora</Link>
       </div>
     </div>
-  )
+    )
+  }
 }
-
-export default Main;
